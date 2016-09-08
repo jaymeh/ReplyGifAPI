7684b756415ab6e7a1601d2f8c1d56be0e22750d
Jamie Sykes <jamie.sykes@creode.co.uk>
2016-09-08 14:21:29 +0100

    Build an API call to return a list of all terms

--- app/Http/Controllers/TermController.php
+++ app/Http/Controllers/TermController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Tag;
+
 class TermController extends Controller
 {
     /**
@@ -16,6 +18,17 @@ class TermController extends Controller
 
     public function allTerms() {
     	// Grab all terms and return them as a json array
+    	$tags = Tag::all();
+
+    	$tag_array = array();
+
+    	foreach($tags as $tag) {
+    		$tag_array[] = array('term' => $tag->tag_name);
+    	}
+
+    	sort($tag_array);
+
+    	return response()->json($tag_array);
     }
 
     public function termById($id) {
